217115b2dbdf7f8f48eacbba309576c2025e9b92
sharchit <sh02archit@gmail.com>
2022-07-27 19:30:37 +0530

    Selection Sort

--- sort.c
+++ sort.c
@@ -22,7 +22,24 @@ void insertionSort(int array[], int size) {
     }
 }
 
-// Driver code
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void selectionSort(int array[], int size) {
+    for (int step = 0; step < size - 1; step++)
+    {
+        int min_idx = step;
+        for (int i = step + 1; i < size; i++)
+        {
+            if (array[i] < array[min_idx])
+                min_idx = i;
+        }
+        swap(&array[min_idx], &array[step]);
+    }
+}
 int main() 
 {
     int n;
@@ -49,6 +66,12 @@ int main()
     {
         insertionSort(arr, n);
         printf("The array after Insertion sort is: ");
+        printArray(arr, n);
+    }
+    else if(key == 2)
+    {
+        selectionSort(arr, n);
+        printf("The array after Selection sort is: ");
         printArray(arr, n);
     }
     else
